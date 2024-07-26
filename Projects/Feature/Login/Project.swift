6b97a4c707de15148fe8c08aa62a0d91d3060782
김용재 <kim-yongjae@gim-yongjaeui-MacBookPro.local>
2024-07-26 15:49:31 +0900

    chore: FeatureLogin example에 info plist 설정

--- Projects/Feature/Login/Project.swift
+++ Projects/Feature/Login/Project.swift
@@ -42,6 +42,22 @@ let project = Project.makeModule(
         .feature(
             example: .Login,
             factory: .init(
+                infoPlist: .extendingDefault(
+                    with: [
+                        "BASE_URL": "http://223.130.130.31:8080",
+                        "CFBundleShortVersionString": "1.0",
+                        "CFBundleVersion": "1",
+                        "CFBundleName": "MissionMate",
+                        "CFBundleIconName": "AppIcon",
+                        "UILaunchStoryboardName": "LaunchScreen.storyboard",
+                        "UIApplicationSceneManifest": [
+                            "UIApplicationSupportsMultipleScenes": false,
+                            "UISceneConfigurations": []
+                        ],
+                        "NSAppTransportSecurity": [
+                            "NSAllowsArbitraryLoads": true
+                        ]
+                    ]),
                 dependencies: [
                     .feature(interface: .Login)
                 ]
