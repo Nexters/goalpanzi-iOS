222ad61fe9b647af6b3bfbba6e9fb31cb2cc04e3
longlivedrgn <kimye9808@naver.com>
2024-08-02 01:37:46 +0900

    chore: PieceView UI Component간 간격 조정

--- Projects/Feature/PieceCreation/Interface/Sources/PieceCreationView.swift
+++ Projects/Feature/PieceCreation/Interface/Sources/PieceCreationView.swift
@@ -21,75 +21,73 @@ public struct PieceCreationView: View {
     }
 
     public var body: some View {
-        ZStack {
-            Color.clear
-                .contentShape(Rectangle())
-                .onTapGesture {
-                    isTextFieldFocused = false
-                }
+        VStack {
+            Text("프로필 만들기")
+                .font(.pretendard(kind: .heading_sm, type: .bold))
+                .foregroundColor(MissionMateColor.gray1)
+                .padding(.top, 48)
 
-            VStack {
-                Text("프로필 만들기")
-                    .font(.pretendard(kind: .heading_sm, type: .bold))
-                    .foregroundColor(MissionMateColor.gray1)
+            Spacer()
 
-                Spacer().frame(height: 48)
+            Image(uiImage: store.selectedPiece.roundImage)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 220, height: 220)
+                .padding(.top, 48)
 
-                Image(uiImage: store.selectedPiece.roundImage)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 220, height: 220)
-
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 8) {
-                        Spacer().frame(width: 16)
-                        ForEach(Piece.allCases, id: \.self) { piece in
-                            VStack {
-                                Image(uiImage: piece.basicImage)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .opacity(piece == store.selectedPiece ? 1.0 : 0.3)
-                                    .onTapGesture {
-                                        store.send(.pieceImageTapped(piece))
-                                    }
-                                Text(piece.koreanName)
-                                    .font(.pretendard(kind: .body_md, type:.bold))
-                                    .frame(height: 24)
-                                    .padding(.horizontal, 25)
-                                    .foregroundColor(MissionMateColor.gray2)
-                                    .background(MissionMateColor.gray5)
-                                    .opacity(piece == store.selectedPiece ? 1.0 : 0.3)
-                                    .cornerRadius(20)
-                            }
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 8) {
+                    Spacer().frame(width: 16)
+                    ForEach(Piece.allCases, id: \.self) { piece in
+                        VStack {
+                            Image(uiImage: piece.basicImage)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .opacity(piece == store.selectedPiece ? 1.0 : 0.3)
+                                .onTapGesture {
+                                    store.send(.pieceImageTapped(piece))
+                                }
+                            Text(piece.koreanName)
+                                .font(.pretendard(kind: .body_md, type:.bold))
+                                .frame(height: 24)
+                                .padding(.horizontal, 25)
+                                .foregroundColor(MissionMateColor.gray2)
+                                .background(MissionMateColor.gray5)
+                                .opacity(piece == store.selectedPiece ? 1.0 : 0.3)
+                                .cornerRadius(20)
                         }
                     }
                 }
-
-                Spacer().frame(height: 18)
-
-                MissionMateTextField(
-                    text: $store.nickName,
-                    isValidInputText: $store.isValidNickName,
-                    placeholder: "닉네임 입력",
-                    noticeMessage: "1~6자, 한글, 영문 또는 숫자를 입력하세요."
-                )
-                .focused($isTextFieldFocused)
-                .padding(.horizontal, 24)
-
-                Spacer().frame(height: 70)
-
-                MissionMateRoundedButton(isDisabled: $store.isAllCompleted, title: "저장하기") {
-                    print("Button Tapped")
-                }
-                .frame(maxWidth: .infinity) // Ensure button stretches horizontally
-                .frame(height: 60)
-                .padding(.horizontal, 24)
-                .padding(.bottom, 36)
             }
+            .padding(.top, 16)
+
+            Spacer()
+
+            MissionMateTextField(
+                text: $store.nickName,
+                isValidInputText: $store.isValidNickName,
+                placeholder: "닉네임 입력",
+                noticeMessage: "1~6자, 한글, 영문 또는 숫자를 입력하세요."
+            )
+            .focused($isTextFieldFocused)
+            .padding(.horizontal, 24)
+
+            Spacer()
+
+            MissionMateRoundedButton(isDisabled: $store.isAllCompleted, title: "저장하기") {
+                print("Button Tapped")
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 60)
+            .padding(.horizontal, 24)
+            .padding(.bottom, 36)
         }
         .padding(.bottom, keyboardResponder.currentHeight)
         .edgesIgnoringSafeArea(.bottom)
         .animation(.easeOut(duration: 0.3), value: keyboardResponder.currentHeight)
+        .onTapGesture {
+            isTextFieldFocused = false
+        }
     }
 }
