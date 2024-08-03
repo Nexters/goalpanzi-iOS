bb13ae09af5c284d02865d7c9f5a4ebd2a127dd2
haeseoklee <haeseoklee.dev@gmail.com>
2024-08-03 20:41:44 +0900

    feat: navigation 구현

--- Projects/App/Sources/RootFeature.swift
+++ Projects/App/Sources/RootFeature.swift
@@ -14,19 +14,44 @@ struct RootFeature {
     @ObservableState
     struct State {
         @Presents var destination: RootDestination.State?
+        var path: StackState<RootPath.State>
         
-        init(destination: RootDestination.State? = nil) {
-            self.destination = .login(LoginFeature.State())
+        init(destination: RootDestination.State? = nil, path: StackState<RootPath.State> = .init()) {
+            self.destination = destination
+            self.path = path
         }
     }
     
     enum Action {
         case destination(PresentationAction<RootDestination.Action>)
+        case path(StackActionOf<RootPath>)
+        case didLoad
+        case goToLogin
     }
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
-            return .none
+            switch action {
+            case .didLoad:
+                return .run { send in
+                    do {
+                        try await Task.sleep(2_000_000_000)
+                        print("task sleep end")
+                        await send(.goToLogin)
+                    } catch {
+                        print("!!!")
+                    }
+                }
+            case .destination:
+                return .none
+                
+            case .path:
+                return .none
+                
+            case .goToLogin:
+                state.destination = .login(LoginFeature.State())
+                return .none
+            }
         }
         .ifLet(\.$destination, action: \.destination)
     }
