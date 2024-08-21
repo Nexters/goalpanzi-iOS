a5180d954b4cc08e8b1e18d6083effa3966b47be
haeseoklee <haeseoklee.dev@gmail.com>
2024-08-21 22:44:27 +0900

    fix: LazyVGrid 로 리팩토링

--- Projects/Feature/Home/Interface/Sources/Home/Views/BoardView.swift
+++ Projects/Feature/Home/Interface/Sources/Home/Views/BoardView.swift
@@ -24,27 +24,25 @@ struct BoardView: View {
     var body: some View {
         let numberOfRows: Int = store.competition?.board.numberOfRows ?? 0
         let numberOfColumns: Int = store.competition?.board.numberOfColumns ?? 0
-        
-        return Grid(horizontalSpacing: 0, verticalSpacing: 0) {
+        let columns: [GridItem] = Array(repeating: .init(.flexible(), spacing: 0), count: numberOfColumns)
+        return LazyVGrid(columns: columns, alignment: .center, spacing: 0) {
             ForEach(0..<numberOfRows, id: \.self) { row in
-                GridRow {
-                    let indices = Array((0..<numberOfColumns))
-                    ForEach((row % 2 == 0) ? indices : indices.reversed(), id: \.self) { col in
-                        let index = col + (row * numberOfColumns)
-                        let position = Position(index: index)
-                        BlockView(
-                            store: store,
-                            block: store.competition?.board.findBlock(by: position),
-                            event: store.competition?.board.findEvent(by: position),
-                            representativePiece: store.competition?.representativePiece(by: position),
-                            numberOfSamePositionPieces: store.competition?.board.findPieces(by: position).count ?? 0,
-                            movingPiece: store.movingPiece?.position == position ? store.movingPiece : nil,
-                            shouldShowMovingPiece: $shouldShowMovingPiece,
-                            movingDirection: $movingDirection
-                        )
-                        .zIndex(-Double(index))
-                        .id(index)
-                    }
+                let indices = Array((0..<numberOfColumns))
+                ForEach((row % 2 == 0) ? indices : indices.reversed(), id: \.self) { col in
+                    let index = col + (row * numberOfColumns)
+                    let position = Position(index: index)
+                    BlockView(
+                        store: store,
+                        block: store.competition?.board.findBlock(by: position),
+                        event: store.competition?.board.findEvent(by: position),
+                        representativePiece: store.competition?.representativePiece(by: position),
+                        numberOfSamePositionPieces: store.competition?.board.findPieces(by: position).count ?? 0,
+                        movingPiece: store.movingPiece?.position == position ? store.movingPiece : nil,
+                        shouldShowMovingPiece: $shouldShowMovingPiece,
+                        movingDirection: $movingDirection
+                    )
+                    .zIndex(-Double(index))
+                    .id(index)
                 }
             }
         }
