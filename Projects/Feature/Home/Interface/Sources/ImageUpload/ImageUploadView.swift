058dd7f497e36bfb50099e74a359ad4cc3f73639
haeseoklee <haeseoklee.dev@gmail.com>
2024-08-22 01:57:25 +0900

    fix: 이미지 업로드 ui 수정

--- Projects/Feature/Home/Interface/Sources/ImageUpload/ImageUploadView.swift
+++ Projects/Feature/Home/Interface/Sources/ImageUpload/ImageUploadView.swift
@@ -13,62 +13,72 @@ public struct ImageUploadView: View {
     
     let store: StoreOf<ImageUploadFeature>
     
+    public init(store: StoreOf<ImageUploadFeature>) {
+        self.store = store
+    }
+    
     public var body: some View {
-        VStack(spacing: 0) {
-            ZStack(alignment: .bottom) {
-                ZStack(alignment: .topLeading) {
-                    Image(uiImage: store.selectedImage)
-                        .resizable()
-                        .edgesIgnoringSafeArea(.all)
-                    
-                    HStack(alignment: .center, spacing: 8) {
-                        SharedDesignSystemAsset.Images.roundBorderRabbit.swiftUIImage
+        ZStack(alignment: .bottom) {
+            ZStack(alignment: .top) {
+                SharedDesignSystemAsset.Colors.white.swiftUIColor
+                    .edgesIgnoringSafeArea(.all)
+                    .overlay {
+                        Image(uiImage: store.selectedImage)
                             .resizable()
-                            .frame(width: 28, height: 28)
-                            .clipShape(Circle())
-                        
-                        Text(store.player.name)
-                            .font(.pretendard(kind: .body_xl, type: .bold))
-                            .foregroundColor(SharedDesignSystemAsset.Colors.white.swiftUIColor)
-                        
-                        Text(store.formatedDate)
-                            .font(.pretendard(kind: .body_xl, type: .light))
-                            .foregroundColor(SharedDesignSystemAsset.Colors.white.swiftUIColor)
-                        
-                        Spacer()
-                        
-                        Button(action: {
-                            store.send(.didTapCloseButton)
-                        }) {
-                            SharedDesignSystemAsset.Images.closeRound.swiftUIImage
-                                .resizable()
-                                .renderingMode(.template)
-                                .foregroundColor(SharedDesignSystemAsset.Colors.white.swiftUIColor)
-                                .frame(width: 40, height: 40)
-                        }
-                        
+                            .scaledToFill()
+                            .edgesIgnoringSafeArea([.horizontal, .bottom])
                     }
-                    .padding(.top, 14)
-                    .padding(.horizontal, 24)
-                }
-                .overlay {
-                    ProgressView()
-                        .progressViewStyle(CircularProgressViewStyle())
-                        .isHidden(!store.isLoading, remove: true)
-                }
-                Button(action: {
-                    store.send(.didTapUploadButton)
-                }) {
-                    Text("업로드")
-                        .font(.pretendard(kind: .body_lg, type: .bold))
+                    .overlay {
+                        ProgressView()
+                            .progressViewStyle(CircularProgressViewStyle())
+                            .isHidden(!store.isLoading, remove: true)
+                    }
+                
+                LinearGradient(gradient: Gradient(colors: [.black.opacity(0.4), .clear]), startPoint: .top, endPoint: .bottom)
+                    .frame(height: 93)
+                
+                HStack(alignment: .center, spacing: 8) {
+                    store.player.character.roundBorderImage.swiftUIImage
+                        .resizable()
+                        .frame(width: 28, height: 28)
+                        .clipShape(Circle())
+                    
+                    Text(store.player.name)
+                        .font(.pretendard(kind: .body_xl, type: .bold))
                         .foregroundColor(SharedDesignSystemAsset.Colors.white.swiftUIColor)
-                        .frame(height: 60)
-                        .frame(maxWidth: .infinity)
-                        .background(SharedDesignSystemAsset.Colors.orange.swiftUIColor)
-                        .cornerRadius(30)
+                    
+                    Text(store.formatedDate)
+                        .font(.pretendard(kind: .body_xl, type: .light))
+                        .foregroundColor(SharedDesignSystemAsset.Colors.white.swiftUIColor)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        store.send(.didTapCloseButton)
+                    }) {
+                        SharedDesignSystemAsset.Images.closeRound.swiftUIImage
+                            .resizable()
+                            .renderingMode(.template)
+                            .foregroundColor(SharedDesignSystemAsset.Colors.white.swiftUIColor)
+                            .frame(width: 40, height: 40)
+                    }
                 }
+                .frame(height: 40)
+                .padding(.top, 14)
                 .padding(.horizontal, 24)
             }
+            Button(action: {
+                store.send(.didTapUploadButton)
+            }) {
+                Text("업로드")
+                    .font(.pretendard(kind: .body_lg, type: .bold))
+                    .foregroundColor(SharedDesignSystemAsset.Colors.white.swiftUIColor)
+                    .frame(height: 60)
+                    .frame(maxWidth: .infinity)
+                    .background(SharedDesignSystemAsset.Colors.orange.swiftUIColor)
+                    .cornerRadius(30)
+            }
+            .padding(.horizontal, 24)
         }
     }
 }
