909896e003b7a99ab8bf755424b29f6f8624a1e0
haeseoklee <haeseoklee.dev@gmail.com>
2024-08-22 00:05:39 +0900

    fix: 이미지 디테일 UI 수정

--- Projects/App/Sources/Previews/HomePreview.swift
+++ Projects/App/Sources/Previews/HomePreview.swift
@@ -9,10 +9,18 @@ import SwiftUI
 import DomainPlayerInterface
 import FeatureHomeInterface
 
+//struct HomeContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HomeView(store: .init(initialState: HomeFeature.State(), reducer: {
+//            HomeFeature()
+//        }))
+//    }
+//}
+
 struct HomeContentView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView(store: .init(initialState: HomeFeature.State(), reducer: {
-            HomeFeature()
+        ImageDetailView(store: .init(initialState: ImageDetailFeature.State(player: .init(id: "", pieceID: "", name: "하이", character: .rabbit), verifiedAt: Date.now, imageURL: "https://dummyimage.com/900x700/663399/fff"), reducer: {
+            ImageDetailFeature()
         }))
     }
 }
