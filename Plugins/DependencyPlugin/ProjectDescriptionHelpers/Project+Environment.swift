a9a5e3077b6a723a504929202cb983d926f637ad
haeseoklee <haeseoklee.dev@gmail.com>
2024-10-28 23:29:02 +0900

    feat: 최소버전 17->18

--- Plugins/DependencyPlugin/ProjectDescriptionHelpers/Project+Environment.swift
+++ Plugins/DependencyPlugin/ProjectDescriptionHelpers/Project+Environment.swift
@@ -4,7 +4,7 @@ import ProjectDescription
 public extension Project {
     enum Environment {
         public static let appName = "MissionMate"
-        public static let deploymentTarget = DeploymentTargets.iOS("17.0")
+        public static let deploymentTarget = DeploymentTargets.iOS("18.0")
         public static let bundlePrefix = "com.goalpanzi.missionmate"
     }
 }
