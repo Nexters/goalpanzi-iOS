a1f2ef4cd9a99c12145c9ca13e72487955cd28b5
longlivedrgn <kimye9808@naver.com>
2025-01-26 16:07:40 +0900

    chore: 앱 버전 1.2으로 변경

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -9,7 +9,7 @@ let targets: [Target] = [
             infoPlist: .extendingDefault(
                 with: [
                     "BASE_URL": "https://mission-mate.kro.kr/",
-                    "CFBundleShortVersionString": "1.0",
+                    "CFBundleShortVersionString": "1.2",
                     "CFBundleVersion": "1",
                     "CFBundleName": "MissionMate",
                     "CFBundleDisplayName": "미션메이트",
@@ -20,7 +20,8 @@ let targets: [Target] = [
                         "UISceneConfigurations": []
                     ],
                     "UIUserInterfaceStyle": "Light",
-                    "UISupportedInterfaceOrientations": ["UIInterfaceOrientationPortrait"]
+                    "UISupportedInterfaceOrientations": ["UIInterfaceOrientationPortrait"],
+                    "ITSAppUsesNonExemptEncryption": false
                 ]),
             entitlements: "MissionMate.entitlements",
             dependencies: [
--- Projects/Feature/Setting/Project.swift
+++ Projects/Feature/Setting/Project.swift
@@ -47,8 +47,9 @@ let project = Project.makeModule(
                 infoPlist: .extendingDefault(
                     with: [
                         "BASE_URL": "https://mission-mate.kro.kr/",
-                        "CFBundleShortVersionString": "1.0",
+                        "CFBundleShortVersionString": "1.2",
                         "CFBundleVersion": "1",
+                        "ITSAppUsesNonExemptEncryption": false,
                         "CFBundleName": "MissionMate",
                         "CFBundleIconName": "AppIcon",
                         "UILaunchStoryboardName": "LaunchScreen.storyboard",
